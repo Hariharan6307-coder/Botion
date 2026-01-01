2fee5accf49f62e2d04b6caa79bc9e616957756c
Hariharan <hariharan.m6307@gmail.com>
2026-01-02 01:50:19 +0530

    It connects to the database (It aint much, but it's honest work :)

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,33 +1,22 @@
 package main
 
 import (
-	"context"
-	"fmt"
+	"backend/internal/config"
+	"backend/internal/database"
 	"log"
-	"os"
-
-	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	// 1. HARDCODE for now to test (We will move this to an env variable later)
-	// REPLACE THIS STRING with the "DB URL" you copied from 'supabase status'
-	dbUrl := "postgresql://postgres:postgres@127.0.0.1:54322/postgres"
-
-	// 2. Connect to the database
-	conn, err := pgx.Connect(context.Background(), dbUrl)
+	cfg, err := config.Load()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	// Close the connection when main() finishes
-	defer conn.Close(context.Background())
-
-	// 3. Test the connection with a simple Ping
-	err = conn.Ping(context.Background())
-	if err != nil {
-		log.Fatal("Could not ping database")
+		log.Fatal("Failed to load config:", err)
 	}
 
-	fmt.Println("✅ Successfully connected to local Supabase Postgres!")
+	db, err := database.NewConnectionPool(cfg.Database)
+	if err != nil {
+		log.Fatal("Failed to connect to database");
+	}
+	defer db.Close()
+
+	log.Println("Database connection established successfully")
 }
